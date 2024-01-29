407827a2ac8b17214f07e2fccf6d06956372ad06
AliHakim773 <alihakim7732@gmail.com>
2024-01-29 19:02:05 +0200

    Server: Connected to mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+require("dotenv").config()
 const express = require("express")
+const connectToMongoDB = require("./configs/db.config")
 
 const app = express()
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log("Server listining on PORT: ", port)
+
+  connectToMongoDB()
 })
