211b349a0f2d1fe4ede7db8bf9808c47cecef51e
AliHakim773 <alihakim7732@gmail.com>
2024-01-29 22:10:56 +0200

    Server: Used the errorHandler middleware

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,12 +1,12 @@
 const User = require("../models/user.model")
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
   const { username, email, password } = req.body
   try {
     await User.create({ username, email, password })
-    res.status(200).send({ message: "User created successfully" })
+    res.status(201).send({ message: "User created successfully" })
   } catch (error) {
-    res.status(500).send({ error })
+    next(error)
   }
 }
 
