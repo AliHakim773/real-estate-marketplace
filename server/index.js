49fcf60986faaad4e2802c073115ae942ee13d36
AliHakim773 <alihakim7732@gmail.com>
2024-01-29 18:51:16 +0200

    Server: Added .env.example file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log("Server listining on PORT: ", port)
 })
