480b2c12630a2f6b853df08ed4181f010279fb58
AliHakim773 <alihakim7732@gmail.com>
2024-01-29 22:12:19 +0200

    Server: Updated errorHandlerMiddleware function name

--- server/middlewares/errorHandler.middleware.js
+++ server/middlewares/errorHandler.middleware.js
@@ -1,7 +1,7 @@
-const errorMiddleware = (error, req, res, next) => {
+const errorHandlerMiddleware = (error, req, res, next) => {
   const statusCode = error.statusCode || 500
   const message = error.message || "Internal Server Error"
   return res.status(statusCode).send({ success: false, message, statusCode })
 }
 
-module.exports = errorMiddleware
+module.exports = errorHandlerMiddleware
