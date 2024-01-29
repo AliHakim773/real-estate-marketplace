f442a897c1134e7dcb36b5ccc5794a8cad38873d
AliHakim773 <alihakim7732@gmail.com>
2024-01-29 18:02:47 +0200

    Client: added routes for the created pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,39 @@
+// React
 import { FC } from "react"
 
+// React Router
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom"
+
+// Pages
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'>
+      <Route path='home' element={<Home />} />
+      <Route path='sign-in' element={<SignIn />} />
+      <Route path='sign-up' element={<SignUp />} />
+      <Route path='about' element={<About />} />
+      <Route path='profile' element={<Profile />} />
+    </Route>
+  )
+)
+
 const App: FC = () => {
-  return <div>Hello</div>
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
