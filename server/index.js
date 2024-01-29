64013d2076814ee1faf9fbbe03de8ca8dd5502ef
AliHakim773 <alihakim7732@gmail.com>
2024-01-29 22:14:14 +0200

    Server: Rearanged lines

--- server/index.js
+++ server/index.js
@@ -13,11 +13,12 @@ app.use(express.json())
 // Initialize app routes
 siteRoutes(app)
 
+// Add global error handler
+app.use(errorHandlerMiddleware)
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log("Server listining on PORT: ", port)
 
   connectToMongoDB()
 })
-
-app.use(errorHandlerMiddleware)
