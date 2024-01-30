d39f90e17f5aa6d6362cd5d5fba6db77b07eafda
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 15:26:09 +0200

    Client: Added sendRequest axios function

---
+++ client/src/cors/axios/index.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from "axios"
+import { ISendRequest } from "../types/axiosTypes"
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+export const sendRequest = async <T>({
+  route,
+  method = "GET",
+  body,
+}: ISendRequest<T>): Promise<T> => {
+  const response: AxiosResponse<T> = await axios.request({
+    url: route,
+    method,
+    data: body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+
+  return response.data
+}
