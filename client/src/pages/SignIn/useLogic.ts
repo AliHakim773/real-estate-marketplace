fe83a4ff92360e77c478971a7e3a3f18347b044e
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 18:42:46 +0200

    Client: Added redux to signIn logic

--- client/src/pages/SignIn/useLogic.ts
+++ client/src/pages/SignIn/useLogic.ts
@@ -3,11 +3,18 @@ import { IRequestError } from "../../cors/types/axiosTypes"
 import { ISignInData } from "../../cors/types/authTypes"
 import authAPI from "../../cors/apis/auth"
 import { useNavigate } from "react-router-dom"
+import { useAppDispatch, useAppSelector } from "../../cors/hooks/redux"
+import {
+  extractUserSlice,
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../../cors/redux/userSlice"
 
 const useLogic = () => {
   const navigate = useNavigate()
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<IRequestError | null>(null)
+  const dispatch = useAppDispatch()
+  const { error, loading } = useAppSelector(extractUserSlice)
   const [formData, SetFormData] = useState<ISignInData>({
     username: "",
     password: "",
@@ -18,26 +25,26 @@ const useLogic = () => {
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    setLoading(true)
+
     try {
-      const res = await authAPI.signIn(formData)
-      console.log(res)
-      setError(null)
-      setLoading(false)
+      dispatch(signInStart())
+      const user = await authAPI.signIn(formData)
+      dispatch(signInSuccess(user))
       navigate("/")
     } catch (e: any) {
       if (e.response && e.response.data) {
         const apiError: IRequestError = e.response.data
-        setError(apiError)
+        dispatch(signInFailure(apiError))
       } else {
-        setError({
-          message: "An unexpected error occurred.",
-          statusCode: 500,
-          success: false,
-        })
+        dispatch(
+          signInFailure({
+            message: "An unexpected error occurred.",
+            statusCode: 500,
+            success: false,
+          })
+        )
       }
     }
-    setLoading(false)
   }
   return { handleSubmit, handleChange, error, loading }
 }
