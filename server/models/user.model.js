db86c2824addd21b8e812099892befd8f7512130
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 20:17:29 +0200

    Server: Added avatar attribute to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -16,6 +16,11 @@ const UserSchema = new mongoose.Schema(
       trim: true,
     },
     password: { type: String, required: "Password is required", trim: true },
+    avatar: {
+      type: String,
+      default:
+        "https://isobarscience-1bfd8.kxcdn.com/wp-content/uploads/2020/09/default-profile-picture1.jpg",
+    },
   },
   { timestamps: true }
 )
