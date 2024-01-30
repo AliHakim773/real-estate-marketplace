66f9e2af0468960a8f05422a66e4b19bccf321e6
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 16:40:38 +0200

    Client: Added signIn api call

--- client/src/cors/apis/auth.ts
+++ client/src/cors/apis/auth.ts
@@ -1,10 +1,18 @@
 import { sendRequest } from "../axios"
-import { ISignUpData } from "../types/authTypes"
+import { ISignInData, ISignUpData } from "../types/authTypes"
 
 const authAPI = {
   signUp: async (data: ISignUpData) => {
     const res = await sendRequest({
       route: "auth/signup",
+      method: "POST",
+      body: data,
+    })
+    return res
+  },
+  signIn: async (data: ISignInData) => {
+    const res = await sendRequest({
+      route: "auth/signin",
       method: "POST",
       body: data,
     })
