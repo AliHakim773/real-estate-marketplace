ad6f1c62be23436435c810734a701cdd1d5a209d
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 20:20:57 +0200

    Client: Added OAuth component UI

--- client/src/components/OAuth/index.tsx
+++ client/src/components/OAuth/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react"
 
 const OAuth: FC = () => {
-  return <div></div>
+  return (
+    <button
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+      Continue with Google
+    </button>
+  )
 }
 
 export default OAuth
