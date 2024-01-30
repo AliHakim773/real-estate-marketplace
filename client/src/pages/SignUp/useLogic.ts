5267fec85b43e1cf6f0f1795e11fc6396bd18aa1
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 16:42:07 +0200

    Client: removed console.log and added navigation to signin after complete signup

--- client/src/pages/SignUp/useLogic.ts
+++ client/src/pages/SignUp/useLogic.ts
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom"
 import { ChangeEvent, FormEvent, useState } from "react"
 import { IRequestError } from "../../cors/types/axiosTypes"
 import { ISignUpData } from "../../cors/types/authTypes"
 import authAPI from "../../cors/apis/auth"
 
 const useLogic = () => {
+  const navigate = useNavigate()
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<IRequestError | null>(null)
   const [formData, SetFormData] = useState<ISignUpData>({
@@ -13,16 +15,18 @@ const useLogic = () => {
   })
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     SetFormData({ ...formData, [e.target.id]: e.target.value })
-    console.log(formData)
   }
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setLoading(true)
+
     try {
-      const res = await authAPI.signUp(formData)
-      console.log(res)
+      await authAPI.signUp(formData)
+
+      setLoading(false)
       setError(null)
+      navigate("sign-in")
     } catch (e: any) {
       if (e.response && e.response.data) {
         const apiError: IRequestError = e.response.data
