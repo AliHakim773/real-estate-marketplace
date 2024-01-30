2f6b40fd49ffc78af233f6c1fc983bcdd7dec65f
AliHakim773 <alihakim7732@gmail.com>
2024-01-30 18:56:49 +0200

    Client: Configured redux persist

--- client/src/cors/redux/store.ts
+++ client/src/cors/redux/store.ts
@@ -1,13 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { persistReducer, persistStore } from "redux-persist"
 import userSlice, { user } from "./userSlice"
+import storage from "redux-persist/lib/storage"
+
+const rootReducer = combineReducers({ [user]: userSlice })
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    [user]: userSlice,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 })
 
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
