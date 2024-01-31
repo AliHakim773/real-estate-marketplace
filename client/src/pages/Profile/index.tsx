ba0bf59777f0bdcec7a4f15feda63761cb9f774b
AliHakim773 <alihakim7732@gmail.com>
2024-01-31 14:31:09 +0200

    Client: Added link to create-listing

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, FC } from "react"
 import useLogic from "./useLogic"
+import { Link } from "react-router-dom"
 
 const Profile: FC = () => {
   const {
@@ -82,6 +83,11 @@ const Profile: FC = () => {
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          to={"/create-listing"}
+          className='bg-green-700 text-white text-center rounded-lg p-3 uppercase hover:opacity-95'>
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span
