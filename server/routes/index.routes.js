3b727c3f5ab501462bac430646f5939a935f250d
AliHakim773 <alihakim7732@gmail.com>
2024-01-31 13:09:27 +0200

    Server: Added listing routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,15 +1,18 @@
 const authMiddleware = require("../middlewares/auth.middleware")
-const authRoutes = require("./auth.routes")
-const userRoutes = require("./user.routes")
+const authRouter = require("./auth.routes")
+const userRouter = require("./user.routes")
+const listingRouter = require("./listing.routes")
 
 const siteRoutes = (app) => {
   // healthy check route
   app.get("/api/v1/healthy", (req, res) => res.status(200).send(true))
 
   // auth routes
-  app.use("/api/v1/auth", authRoutes)
+  app.use("/api/v1/auth", authRouter)
   // user routes
-  app.use("/api/v1/user", authMiddleware, userRoutes)
+  app.use("/api/v1/user", authMiddleware, userRouter)
+  // listing routes
+  app.use("/api/v1/listing", listingRouter)
 }
 
 module.exports = siteRoutes
